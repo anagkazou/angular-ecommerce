b02437eb0c06e61e18fe314ca4c788581d913d86
davidomole <thedavidomole#gmail.com>
2021-05-20 16:56:23 +0100

    generate pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,71 @@ import { initializeApp } from 'firebase/app';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { OrderSucessComponent } from './components/order-sucess/order-sucess.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/components/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/components/admin-orders/admin-orders.component';
+
+import { RouterModule } from '@angular/router';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    OrderSucessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'shopping-cart',
+        component: ShoppingCartComponent,
+      },
+      {
+        path: 'check-out',
+        component: CheckOutComponent,
+      },
+      {
+        path: 'order-success',
+        component: OrderSucessComponent,
+      },
+      {
+        path: 'customer/orders',
+        component: MyOrdersComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'admin/products',
+        component: AdminProductsComponent,
+      },
+      {
+        path: 'admin/order',
+        component: AdminOrdersComponent,
+      },
+    ]),
     AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
